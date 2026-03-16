ac3767dbba74da285574d5a883af651a8969cef0
André Siqueira <siqueira.andre@gmail.com>
2026-03-15 21:33:24 -0300

    chore: implementa DatabaseSeeder com dados reais (Missas, Etapas e Catequistas)

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -8,8 +8,6 @@ use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    use WithoutModelEvents;
-
     /**
      * Seed the application's database.
      */
@@ -18,8 +16,45 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Admin User',
+            'email' => 'admin@admin.com',
+            'password' => bcrypt('password'),
+        ]);
+
+        // Criando Missas
+        $missas = [
+            'Sábado - 18h',
+            'Domingo - 8h',
+            'Domingo - 9h30',
+            'Domingo - 11h',
+            'Domingo - 19h30',
+        ];
+
+        foreach ($missas as $missa) {
+            \App\Models\Missa::create(['descricao' => $missa]);
+        }
+
+        // Criando Etapas
+        $etapa1 = \App\Models\Etapa::create(['nome' => '1ª etapa']);
+        $etapa2 = \App\Models\Etapa::create(['nome' => '2ª etapa']);
+        $etapa3 = \App\Models\Etapa::create(['nome' => '3ª etapa']);
+
+        // Criando Catequistas da 1ª Etapa
+        $etapa1->catequistas()->createMany([
+            ['nomes' => 'Eduardo e Vivian'],
+            ['nomes' => 'Fernando e Diana'],
+        ]);
+
+        // Criando Catequistas da 2ª Etapa
+        $etapa2->catequistas()->createMany([
+            ['nomes' => 'Nani e Rosana'],
+            ['nomes' => 'Cristiane e Patrícia'],
+        ]);
+
+        // Criando Catequistas da 3ª Etapa
+        $etapa3->catequistas()->createMany([
+            ['nomes' => 'Joana e Fabiana'],
+            ['nomes' => 'Maria e Clara'],
         ]);
     }
 }
